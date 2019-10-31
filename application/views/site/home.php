31995aea368dcab198066fe67cfc437addc801cb
Jopanzo <jpnzo2k12@gmail.com>
2019-10-31 00:08:11 -0400

    Add files via upload

---
+++ application/views/site/home.php
@@ -0,0 +1,9 @@
+<h3>Welcome to the Travel Agency</h3>
+<p>Please enter your username.</p>
+<form class="" action="site/dashboard" method="post">
+  Username:
+  <input type="text" name="" value=""> <br>
+  Email:
+  <input type= "email" name="" value="">
+  <input type="submit" name="" value="">
+</form>
